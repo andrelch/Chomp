f9849c732881cc38a0d1bcf94b4f4894cabe2658
andrelch <andreleung88@gmail.com>
2022-10-25 13:11:13 -0400

    Generated all boards with for loops

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -7,13 +7,13 @@ public class MyPlayer {
     public MyPlayer() {
         columns = new int[10];
 
-        for(int a = 1; a < 4; a++){
+        /*for(int a = 1; a < 4; a++){
             for(int b = 0; b <= a; b++){
                 for(int c = 0; c <= b; c++){
                     System.out.println(a +","+ b +","+ c);
                 }
             }
-        }
+        }*/
 
         /***
          * This code will run just once, when the game opens.
@@ -24,44 +24,6 @@ public class MyPlayer {
     public Point move(Chip[][] pBoard) {
 
         System.out.println("MyPlayer Move");
-
-        //int a = 0;
-        //int b = 0;
-        //int c = 0;
-
-         for(int a = 0; a < 2; a++){
-             for(int b = 0; b < 2; b++){
-                 if(a>=b){
-                     for(int c = 0; c <2; c++){
-                         if(b>=c) {
-                             System.out.println(a + b + c);
-                         }
-                     }
-                 }
-             }
-         }
-
-        /*System.out.println(
-                "333\n" +
-                "332\n" +
-                "331\n" +
-                "330\n" +
-                "322\n" +
-                "321\n" +
-                "320\n" +
-                "311\n" +
-                "310\n" +
-                "300\n" +
-                "222\n" +
-                "221\n" +
-                "220\n" +
-                "211\n" +
-                "210\n" +
-                "200\n" +
-                "111\n" +
-                "110\n" +
-                "100\n"
-        );*/
 
         gameBoard = pBoard;
         int column = 0;
